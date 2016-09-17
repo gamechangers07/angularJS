293d4de1cb840f5e05e0ba5670840dbf518218d2
mike <gamechangers07@gmail.com>
2016-09-17 11:30:06 -0400

    initial commit

---
+++ WebContent/WEB-INF/js/demo.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+function simpleController($scope) {
+
+	$scope.customers = [ {
+		name : 'John Smith',
+		city : 'Madison'
+	}, {
+		name : 'James Knight',
+		city : 'Sanford'
+	}, {
+		name : 'Jane Doe',
+		city : 'Chicago'
+	}, {
+		name : 'Kil Bill',
+		city : 'Milwaukee'
+	}
+	];
+	
+
+}
